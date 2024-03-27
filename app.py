0f33bb3726263094b3d67e888bea7f569ac64130
olusiekwin <olusiekwin@gmail.com>
2024-03-27 17:38:21 +0300

    🔥🔥

--- app.py
+++ app.py
@@ -16,45 +16,36 @@ def ussd_callback():
         text = request.values.get("text", "")
 
         if text == '':
-            session['response'] = "CON Are you persons with disabled or pregnant?\n"
+            session['response'] = "CON Are you persons with disability or pregnant?\n"
             session['response'] += "Reply with:\n"
-            session['response'] += "1. Disabled\n"
-            session['response'] += "2. Pregnant\n"
-            session['response'] += "3. Proceed"
+            session['response'] += "1. Yes\n"
+            session['response'] += "2. No"
 
         elif text == '1':
+            session['response'] = "CON Please specify:\n"
+            session['response'] += "1. Disabled\n"
+            session['response'] += "2. Pregnant"
+
+        elif text == '2':
+            session['response'] = "CON Proceeding to the main menu:\n"
+            session['response'] += "1. Sustainable Farming Education\n"
+            session['response'] += "2. Carbon Credit Awareness\n"
+            session['response'] += "3. Local Resource Mapping\n"
+            session['response'] += "4. Community Engagement"
+
+        elif text == '1*1':
             session['response'] = "CON Since you are disabled, we recommend crops that require less physical effort to maintain:\n"
             session['response'] += "1. Mushroom farming\n"
             session['response'] += "2. Hydroponic farming\n"
             session['response'] += "3. Vertical farming\n"
             session['response'] += "4. Beekeeping"
 
-        elif text == '2':
-            session['response'] = "CON Are you pregnant?\n"
-            session['response'] += "Reply with:\n"
-            session['response'] += "1. Yes\n"
-            session['response'] += "2. No"
-
-        elif text == '2*1':
+        elif text == '1*2':
             session['response'] = "CON Since you are pregnant, we recommend crops that require less attention on the farm:\n"
             session['response'] += "1. Sweet Potatoes\n"
             session['response'] += "2. Pumpkins\n"
             session['response'] += "3. Green Beans\n"
             session['response'] += "4. Lettuce"
-
-        elif text == '2*2':
-            session['response'] = "CON You are not pregnant. Here's the main menu:\n"
-            session['response'] += "1. Sustainable Farming Education\n"
-            session['response'] += "2. Carbon Credit Awareness\n"
-            session['response'] += "3. Local Resource Mapping\n"
-            session['response'] += "4. Community Engagement"
-
-        elif text == '3':
-            session['response'] = "CON Proceeding to the main menu:\n"
-            session['response'] += "1. Sustainable Farming Education\n"
-            session['response'] += "2. Carbon Credit Awareness\n"
-            session['response'] += "3. Local Resource Mapping\n"
-            session['response'] += "4. Community Engagement"
 
     return session.get('response', '')
 
